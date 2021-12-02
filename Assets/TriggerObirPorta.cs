e2e38c9af1ff448bcc5f98659cd56528cf276b8b
EricBenito <ericlulu2002@gmail.com>
2021-12-02 10:44:32 +0100

    hola

---
+++ Assets/TriggerObirPorta.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObirPorta : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "cubu")
+        {
+            Debug.Log("S'ha obert la porta");
+        }
+    }
+
+}
